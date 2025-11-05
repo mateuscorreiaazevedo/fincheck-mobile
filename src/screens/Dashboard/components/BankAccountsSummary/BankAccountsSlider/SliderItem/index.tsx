2f4611beb166295d9f62c28974c52af753738a74
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-11-04 21:05:00 -0300

    refactor: remove shadow from BankAccountsSliderItem component

--- src/screens/Dashboard/components/BankAccountsSummary/BankAccountsSlider/SliderItem/index.tsx
+++ src/screens/Dashboard/components/BankAccountsSummary/BankAccountsSlider/SliderItem/index.tsx
@@ -8,7 +8,7 @@ interface BankAccountsSliderItemProps {
 
 export function BankAccountsSliderItem({ item }: BankAccountsSliderItemProps) {
   return (
-    <View className="h-[204px] w-[260px] justify-between rounded-2xl bg-white p-4 shadow-md">
+    <View className="h-[204px] w-[260px] justify-between rounded-2xl bg-white p-4">
       <Text className="font-600 text-lg">{item.name}</Text>
       <Text className="mt-2 text-base">{item.balance}</Text>
     </View>
